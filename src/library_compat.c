fa5c90eea0b990c2ba39ea67736b0b1c3722d0b6
BenjaminTobilla <benjaminluisangel323@gmail.com>
2025-12-05 20:48:41 -0600

    Update library_compat.c

--- src/library_compat.c
+++ src/library_compat.c
@@ -1,60 +1,23 @@
 #include "library_compat.h"
 
 #ifdef _WIN32
-#include <conio.h>
-#else
-#include <unistd.h>
-#include <termios.h>
-#include <stdio.h>
-#include <fcntl.h>
-#endif
 
 int kbhit_compat() {
-#ifdef _WIN32
     return _kbhit();
-#else
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-
-    return 0;
-#endif
 }
 
 int getch_compat() {
-#ifdef _WIN32
     return _getch();
-#else
-    struct termios oldt, newt;
-    int ch;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    return ch;
-#endif
 }
+
+#else
+
+int kbhit_compat() {
+    return kbhit();
+}
+
+int getch_compat() {
+    return getch_compat();
+}
+
+#endif
